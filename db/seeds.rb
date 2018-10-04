cc742bfdd58cef4b3c2887f44f10bdff7e59d6f1
Michael Wong <mail@michaelwongsilversmith.com>
2018-10-04 12:47:57 +0100

    seeds generated

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,11 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+10.times do |i|
+  puts "Creating request ##{i}"
+  Article.create!(
+  title: Faker::HarryPotter.book,
+  content: Faker::HarryPotter.quote
+  )
+end
